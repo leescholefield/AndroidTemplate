43d3d2dfedbc36202578370763f92586d2e29393
Lee Scholefield <leescholefield@gmail.com>
2017-10-27 20:25:07 +0100

    added getTable(), getWhereClause() and getColumns() methods

--- app/src/main/java/com/scholefield/lee/androidtemplate/db/query/Query.java
+++ app/src/main/java/com/scholefield/lee/androidtemplate/db/query/Query.java
@@ -1,5 +1,7 @@
 package com.scholefield.lee.androidtemplate.db.query;
 
+import android.support.annotation.Nullable;
+
 /**
  * Base class for a SQLite query.
  */
@@ -9,5 +11,22 @@ public interface Query {
      * Returns a SQLite query String.
      */
     String getQuery();
+
+    /**
+     * Returns the SQL table this query should be performed on.
+     */
+    String getTable();
+
+    /**
+     * Returns the SQL where clause or {@code null} if one was not given.
+     */
+    @Nullable
+    String getWhereClause();
+
+    /**
+     * Returns an array of table columns that this query should be performed on. If this is not applicable this will
+     * return an empty array.
+     */
+    String[] getColumns();
 
 }
